ef40e4ed1c4cece28f868043f0d10589fe2543d6
beeMonkey <675383749@qq.com>
2018-07-09 01:55:58 +0800

    finish the top three pratice of collectionOperator

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var arr=[]
+  for(let item of collection){
+    if(item%2==0){
+      arr.push(item)
+    }
+  }
+  return arr
 }
 
 module.exports = collect_all_even;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,25 @@
 'use strict';
-
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let arr=[]
+  if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      if (i % 2 === 0) {
+        arr.push(i)
+      }
+    }
+    return arr
+  }
+  if(number_a > number_b){
+    for(let i=number_a;i>=number_b;i--){
+      if(i%2===0){
+        arr.push(i)
+      }
+    }
+    return arr
+  }
+  if(number_a===number_b){
+    arr.push(number_a)
+    return arr
+  }
 }
-
 module.exports = get_integer_interval_2;
